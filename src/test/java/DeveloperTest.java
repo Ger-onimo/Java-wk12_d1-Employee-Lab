de64ecfec45a3457188eeb72523dd3166a085431
Keith Allan <keithallan01@gmail.com>
2019-02-18 16:17:17 +0000

    developer class created and tested

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+        Developer developer;
+
+        @Before
+        public void before(){
+            developer = new Developer("Joe King", "AB 12 34 56 Z", 27_000);
+        }
+
+        @Test
+        public void hasName(){
+            assertEquals("Joe King", developer.getName());
+        }
+
+        @Test
+        public void hasNIN(){
+            assertEquals("AB 12 34 56 Z", developer.getNI());
+        }
+
+        @Test
+        public void hasSalary(){
+            assertEquals(27_000, developer.getSalary(), 0.01);
+        }
+
+        @Test
+        public void canRaiseSalary(){
+            developer.raiseSalary(20);
+            assertEquals(27_020, developer.getSalary(), 0.01);
+        }
+
+        @Test
+        public void canPayBonus(){
+            assertEquals(270, developer.payBonus(), 0.01);
+        }
+        
 }
